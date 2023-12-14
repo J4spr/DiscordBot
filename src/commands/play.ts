13c720562e1e843d725f9d9ee9fadbd638bc5211
J4spr <jasper7.verbruggen@gmail.com>
2023-12-14 15:39:17 +0100

    added utility subfolder

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -3,7 +3,7 @@ import {CommandInteraction, SlashCommandBuilder} from "discord.js";
 
 export const data = new SlashCommandBuilder()
     .setName("play")
-    .setDescription("Plays a youtube video(audop only)");
+    .setDescription("Plays a youtube video(audio only)");
 
 export async function execute(interaction: CommandInteraction) {
     await interaction.reply("Ayoo dit werkt!");
